b040dca857d2911a36440accfac2171c2130650c
Dark Mode <54356400+spy-way@users.noreply.github.com>
2025-03-09 10:28:13 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
 
 const AdminSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
