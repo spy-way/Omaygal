17cf53717d2145034fec52366fa56ad3b963029a
Dark Mode <54356400+spy-way@users.noreply.github.com>
2025-03-10 13:53:58 +0530

    Update ipBlocker.js

--- middleware/ipBlocker.js
+++ middleware/ipBlocker.js
@@ -3,7 +3,9 @@
 const BannedIP = require('../models/BannedIP');
 
 async function ipBlocker(req, res, next) {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  //  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var ip = req.ip || req.connection.remoteAddress;
+   
     const banned = await BannedIP.findOne({ ip });
     if (banned) {
         res.status(403).send('Your IP has been banned.');
